1e825a90470105512bcec3aa0ed2e24a38ae59b5
NinjaHubHH <nruesch@live.de>
2017-02-20 22:54:47 +0100

    added left n right controller scripts and pointer scripts, for grabbing objects

---
+++ Assets/My_Assets/Models/Scripts/MoveObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+    public float speed; 
+
+	// Use this for initialization
+	void Start () {
+        speed = 7f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
